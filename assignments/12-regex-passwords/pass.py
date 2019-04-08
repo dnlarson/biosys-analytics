a3d2fc605677da8a406ecb216aa4518167e65049
Danielle Larson <daniellelarson@dhcp-10-132-226-239.uawifi.arizona.edu>
2019-04-08 13:35:34 -0700

    added

---
+++ assignments/12-regex-passwords/pass.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Python Password"""
+
+
+import argparse
+import os
+import re
+import sys
+
+
+#-----------------------------------------------------------
+def main():
+    """main"""
+
+    args = sys.argv[1:]
+
+    if len(args) != 2:
+        print('Usage: {} PASSWORD ALT'.format(os.path.basename(sys.argv[0])))
+        sys.exit(1)
+
+    p1, p2 = args
+
+
+
+    print(p1)
+    print(p2)
+
+    if p1 == p2:
+        print('ok')
+    elif p1.capitalize() == p2:
+        print('ok')
+    elif p1.upper() == p2:
+        print('ok')
+    elif re.search('.?' + p1 + '.?', p2):
+        print('ok')
+    else:
+        print('nah')
+
+    
+
+
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
